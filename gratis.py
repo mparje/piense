84ef7bb4da0a2d0b081dbc54c8f1536215b17966
mparje <126131806+mparje@users.noreply.github.com>
2023-04-16 15:17:33 -0600

    Update gratis.py

--- gratis.py
+++ gratis.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import whisper
 import openai
-import streamlit_webrtc as webrtc
+import json
+import requests
 model_engine = "text-davinci-003"
 
 api_key = st.sidebar.text_input("Ingrese su clave de la API de OpenAI", type="password")
@@ -12,48 +13,52 @@ else:
     openai.api_key = api_key
     # Continuar con el resto del código que utiliza la clave de API
 
-# Crear la página de Streamlit
-def main():
-    st.title("Transcripción y ordenamiento de notas de voz")
-    
-    # Agregar la opción para grabar audio en la web
-    webrtc_stream = webrtc.webrtc_streamer(
-        key="audio",
-        mode=webrtc.WebRtcMode.SENDRECV,
-        audio_receiver_size=1024,
-        video_receiver_size="640x480",
-        client_settings={"default_mute": True},
+# Función para transcribir audio usando Whisper
+def transcribe_audio(audio_file):
+    # Cargar el archivo de audio en memoria
+    audio_data = audio_file.read()
+
+    # Realizar una solicitud a la API de OpenAI para transcribir el audio
+    response = requests.post(
+        "https://api.openai.com/v1/engines/whisper-asr/deployed/transcribe",
+        headers={
+            "Content-Type": "audio/wav",
+            "Authorization": f"Bearer {openai.api_key}",
+        },
+        data=audio_data,
     )
-    recording = st.checkbox("Comenzar a grabar")
-    if recording:
-        webrtc_stream.start_recording()
-    else:
-        webrtc_stream.stop_recording()
-    
-    # Si se graba el audio, transcribirlo usando Whisper
-    if webrtc_stream.audio_bytes:
-        with st.spinner("Transcribiendo el audio..."):
-            text = whisper.transcribe_bytes(webrtc_stream.audio_bytes)
-        st.success("La transcripción se ha completado correctamente.")
-        
-        # Ordenar las notas de voz usando el modelo de OpenAI
-        with st.spinner("Ordenando las notas de voz..."):
-            sorted_text = openai.Completion.create(
-                engine=model_engine,
-                prompt=text,
-                max_tokens=1024,
-                n=1,
-                stop=None,
-                temperature=0.5,
-            ).choices[0].text
-        
-        # Mostrar las notas de voz transcritas y ordenadas en la página
-        st.header("Notas de voz transcritas y ordenadas:")
-        st.write(sorted_text)
-    
-    # Mostrar la opción de grabación de audio en la web en la página
-    st.subheader("Grabar audio en la web:")
-    webrtc_stream
-    
-if __name__ == "__main__":
-    main()
+
+    # Extraer el texto transcrito de la respuesta
+    transcribed_text = response.json()["transcription"]
+
+    return transcribed_text
+
+# Función para ordenar notas usando text-davinci-003
+def sort_notes(transcribed_text):
+    prompt = f"Ordena las siguientes notas de manera coherente:\n{transcribed_text}\n"
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    sorted_notes = response.choices[0].text.strip()
+
+    return sorted_notes
+
+# Diseño de la interfaz de usuario de Streamlit
+st.title("Transcriptor y organizador de notas")
+
+uploaded_file = st.file_uploader("Suba su archivo de audio", type=["wav", "mp3"])
+
+if uploaded_file is not None:
+    transcribed_text = transcribe_audio(uploaded_file)
+    st.write("Texto transcrito:", transcribed_text)
+
+    sorted_notes = sort_notes(transcribed_text)
+    st.write("Notas ordenadas:", sorted_notes)
+
