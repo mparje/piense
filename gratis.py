11e8840f10169cd030ed8f830884e6203c12e6d2
mparje <126131806+mparje@users.noreply.github.com>
2023-04-16 22:49:25 -0600

    Update gratis.py

--- gratis.py
+++ gratis.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import openai
 import requests
-import base64
 from pydub import AudioSegment
-from io import BytesIO
 
 # Configurar la clave de la API de OpenAI
 api_key = st.sidebar.text_input("Ingrese su clave de la API de OpenAI", type="password")
@@ -13,45 +11,42 @@ if not api_key:
 else:
     openai.api_key = api_key
 
+    
+    
 
 
-# Configura tu clave API de OpenAI
-openai.api_key = "tu_clave_api"
-
-# Función para transcribir el audio usando Whisper ASR
+# Función para transcribir el audio usando la API de Whisper de OpenAI
 def transcribe_audio(audio_file):
     # Convierte el archivo de audio a formato WAV
     audio = AudioSegment.from_file(audio_file, format="mp3")
-    audio = audio.set_frame_rate(16000).set_channels(1)
     audio.export("audio.wav", format="wav")
     
-    # Carga el archivo de audio en formato WAV
+    # Carga el archivo WAV
     with open("audio.wav", "rb") as f:
         audio_data = f.read()
     
-    # Realiza la transcripción usando la API de OpenAI
-    response = openai.Audio.create(
-        audio=audio_data,
-        model="whisper-asr",
-        sample_rate=16000,
-        format="wav",
-        language="es",
+    # Realiza la transcripción usando la API de Whisper de OpenAI
+    response = requests.post(
+        "https://api.openai.com/v1/engines/whisper-asr/transcribe",
+        headers={
+            "Content-Type": "audio/wav",
+            "Authorization": f"Bearer {openai.api_key}",
+        },
+        data=audio_data,
     )
     
     # Devuelve la transcripción
-    return response["choices"][0]["text"]
+    return response.json()["transcription"]
 
 # Interfaz de Streamlit
-st.title("Transcriptor de audio usando Whisper ASR")
-st.write("Esta aplicación transcribe grabaciones de audio utilizando el modelo Whisper ASR de OpenAI.")
+st.title("Transcriptor de audio con Whisper de OpenAI")
+st.write("Sube un archivo de audio y lo transcribiré usando la API de Whisper de OpenAI.")
 
-uploaded_file = st.file_uploader("Sube un archivo de audio en formato MP3", type=["mp3"])
+uploaded_file = st.file_uploader("Elige un archivo de audio", type=["mp3", "wav"])
 if uploaded_file is not None:
-    st.write("Archivo de audio cargado. Transcribiendo...")
-    try:
+    with st.spinner("Transcribiendo el audio..."):
         transcription = transcribe_audio(uploaded_file)
-        st.write("Transcripción:")
-        st.write(transcription)
-    except Exception as e:
-        st.write("Error al transcribir el archivo de audio.")
-        st.write(str(e))
+    st.write("Transcripción:")
+    st.write(transcription)
+else:
+    st.write("Por favor, sube un archivo de audio.")
